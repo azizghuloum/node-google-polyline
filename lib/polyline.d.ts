11c2784da5cf32955235c52e99479ec5879ed750
Abdulaziz Ghuloum <aghuloum@gmail.com>
2019-08-10 07:40:24 +0300

    fixed type definition

--- lib/polyline.d.ts
+++ lib/polyline.d.ts
@@ -1,6 +1,7 @@
 declare module "google-polyline" {
-  function encode(points: Array<[number, number]>): string;
-  function decode(polyline: string): Array<[number, number]>;
-  export {encode, decode};
+  const polyline: {
+    encode: (points: Array<[number, number]>) => string,
+    decode: (polyline: string) => Array<[number, number]>
+  };
+  export default polyline;
 }
-
